9b418ca90f6edb6f995fc3ef9220278bf7d3c52d
Nathapot <nathapot.po@gmail.com>
2022-01-17 11:21:38 +0700

    first commit

---
+++ bigO/bigO.js
@@ -0,0 +1,25 @@
+// calculates the sum of all numbers from 1 up to n
+
+// 1
+function addUpTo1(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+// 2
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+let t1 = performance.now();
+console.log(addUpTo1(10000000));
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+let t3 = performance.now();
+console.log(addUpTo2(10000000));
+let t4 = performance.now();
+console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`);
